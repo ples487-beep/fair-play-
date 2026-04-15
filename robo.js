b6174710249ba986cfa1c01c257e1f4ad4d84743
Sam-Coelho <samcoelho137@gmail.com>
2026-04-15 18:37:49 +0100

    Update robo.js

--- robo.js
+++ robo.js
@@ -2,9 +2,10 @@ class Robot {
     constructor(x, y) {
       this.x = x;
       this.y = y;
-      this.width = 64;
-      this.height = 160;
+      this.width = 256;
+      this.height = 256;
       this.hitFlash = 0;
+      this.life= 3;
     }
   
     hit() {
@@ -13,7 +14,18 @@ class Robot {
   
     desenhar() {
       fill(this.hitFlash > 0 ? color(255, 0, 0) : color(0));
-      rect(this.x, this.y, this.width, this.height);
+      image(roboStanding,this.x,this.y,this.width,this.height);
+    }
+
+    socar(x,y){
+
+    }
+
+    levarSoco(Cordx,Cordy){
+      if(dist(Cordx,Cordy,this.x,this.y)<100){
+        this.life=this.life-1;
+        print("dano")
+      }
     }
   
     atualizar() {
